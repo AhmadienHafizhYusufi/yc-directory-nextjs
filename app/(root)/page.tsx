eb0b4f20cd32377ad755fd5300ef473e9ffcded4
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-11-17 14:31:08 +0700

    Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -6,12 +14,12 @@ export default function Home() {
           Pitch Your Startup, <br /> Connect With Entrepreneurs
         </h1>
 
-        <p className="sub-heading !max-w-3xl">
+        <p className="sub-heading max-w-3xl!">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
-      </section>
 
-      <h1 className="heading">Home</h1>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
